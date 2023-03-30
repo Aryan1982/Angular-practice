62f89a312e6a3a0a365d3ef2c8683f98625ec9d8
Aryan Thakor <62349184+Aryan1982@users.noreply.github.com>
2023-03-30 10:24:58 +0000

    initial commits

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../Todo";
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit{
+  todos:Todo[];
 
+  constructor(){
+    this.todos=[
+      {
+        sno:1,
+        title:"heya",
+        dsc:"hmmmm",
+        active:true,
+      },
+      {
+        sno:2,
+        title:"heya",
+        dsc:"hmmmm",
+        active:true,
+      },
+      {
+        sno:3,
+        title:"heya",
+        dsc:"hmmmm",
+        active:true,
+      }
+    ]
+  }
+  ngOnInit():void{
+
+  }
 }
